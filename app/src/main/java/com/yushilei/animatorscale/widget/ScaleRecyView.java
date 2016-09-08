f20851a11f3de9ff5336194706755829a9b9044e
yushilei <yushilei.1989@163.com>
2016-09-08 14:55:12 +0800

    RecyclerView 加入注释

--- app/src/main/java/com/yushilei/animatorscale/widget/ScaleRecyView.java
+++ app/src/main/java/com/yushilei/animatorscale/widget/ScaleRecyView.java
@@ -21,54 +21,78 @@ public class ScaleRecyView extends RecyclerView {
         super(context, attrs);
     }
 
+    String TAG = "ScaleRecyView";
+    /**
+     * action down 产生的x,y坐标
+     */
     float lastX;
     float lastY;
-    String TAG = "ScaleRecyView";
+    /**
+     * 当前View是否处于缩放状态
+     */
     boolean isScale = false;
-
+    /**
+     * 当前View的缩放值
+     */
     float mScale = 1.0f;
-
+    /**
+     * 产生缩放时，使用的缩放系数，该值越大缩放比率就越大
+     */
     float scaleRatio = 0.7f;
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
         float x = ev.getX();
         float y = ev.getY();
-        int childCount = getChildCount();
-        Log.d(TAG, "childCount=" + childCount);
+
         switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                //记录Down时的 x y
                 lastX = x;
                 lastY = y;
                 break;
             case MotionEvent.ACTION_MOVE:
-                View view = getChildAt(0);
-                int pos = ((RAdapter.VH) view.getTag()).getPos();
-                int count = getChildCount();
-                if (pos == 0 && view.getTop() >= 0 && y - lastY > 0) {
-                    //向下拉动
-                    float v = y - lastY;
-                    int height = getHeight();
-                    mScale = 1 + v * scaleRatio / height;
-                    setPivotY(0f);
-                    setPivotX(getWidth() / 2);
-                    ViewCompat.setScaleY(this, mScale);
-                    isScale = true;
+                if (y - lastY > 0) {
+                    //获取Recycler的第一个展示的View
+                    View view = getChildAt(0);
+                    int pos = ((RAdapter.VH) view.getTag()).getPos();
+                    //获取该View对应在数据集合中的位置
+                    //如果位置=0 并且该View的上边界=0 并且当前正在向下拉动
+                    if (pos == 0 && view.getTop() >= 0) {
+                        //计算偏移量
+                        float distance = y - lastY;
+                        //计算出要缩放的scale
+                        mScale = 1 + distance * scaleRatio / getHeight();
+                        //设置缩放的锚点
+                        setPivotY(0f);
+                        setPivotX(getWidth() / 2);
+                        //进行缩放
+                        ViewCompat.setScaleY(this, mScale);
+                        isScale = true;
+                    }
+                } else {
+                    //获取最后一个展示的View
+                    int count = getChildCount();
+                    View lastView = getChildAt(count - 1);
+                    int pos = ((RAdapter.VH) lastView.getTag()).getPos();
+                    //判断其是否为数据集合最后一个位置
+                    if (pos + 1 == getAdapter().getItemCount() && lastView.getBottom() <= getBottom()) {
+                        //滑动到最底部 向上拉
+                        float distance = y - lastY;
+                        mScale = 1 - scaleRatio * distance / getHeight();
+                        //设置锚点
+                        setPivotX(getWidth() / 2);
+                        setPivotY(getHeight());
+                        ViewCompat.setScaleY(this, mScale);
+                        isScale = true;
+                    }
                 }
-                View lastView = getChildAt(count - 1);
-                int pos1 = ((RAdapter.VH) lastView.getTag()).getPos();
-                if (pos1 + 1 == getAdapter().getItemCount() && lastView.getBottom() <= getBottom() && y - lastY < 0) {
-                    //滑动到最底部 向上拉
-                    float distance = y - lastY;
-                    mScale = 1 - scaleRatio * distance / getHeight();
-                    setPivotX(getWidth() / 2);
-                    setPivotY(getHeight());
-                    ViewCompat.setScaleY(this, mScale);
-                    isScale = true;
-                }
+
+
                 break;
             case MotionEvent.ACTION_UP:
                 if (isScale) {
+                    //如果已经进行了缩放，up的时候应该恢复原状，从当前scale 恢复至1.0f
                     ObjectAnimator animator = ObjectAnimator.ofFloat(this, "scaleY", mScale, 1.0f);
                     animator.setDuration(300);
                     animator.start();
